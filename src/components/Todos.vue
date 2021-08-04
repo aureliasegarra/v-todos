360f25d2e5d2bd8b87f64817337bc9a3b1a4a62b
aureliasegarra <av.segarra@gmail.com>
2021-08-04 16:09:43 +0200

    initial commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="hello">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: "Hello this is my Todos"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
